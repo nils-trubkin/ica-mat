4461af46dcdf802a8e70de1601b6e57d42199fd1
Nils Trubkin <32000568+nils-trubkin@users.noreply.github.com>
2023-07-01 17:09:36 +0200

    add logic

--- ica4.py
+++ ica4.py
@@ -1,41 +1,46 @@
 import os
 import re
-from PyPDF2 import PdfReader
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 
+from itertools import tee
+from PyPDF2 import PdfReader
+
 # df
-df = None
-
-# Class for item
-class Item:
-    def __init__(self, discount, name, barcode, price, quantity, unit, total, discount_name, discount_total):
-        self.discount = discount
-        self.name = name
-        self.barcode = barcode
-        self.price = price
-        self.quantity = quantity
-        self.unit = unit
-        self.total = total
-        self.discount_name = discount_name
-        self.discount_total = discount_total
-
-    def __str__(self):
-        return f"{self.name} {self.price} {self.quantity} {self.unit} {self.total}"
+input_dir = "src"
+output_dir = "out"
+df_columns = [
+    'Discount',
+    'Name',
+    'Barcode',
+    'Price',
+    'Quantity',
+    'Unit',
+    'Total',
+    'Discount Name',
+    'Discount Total',
+    'Date',
+    'Time',
+    'Store'
+]
+df = pd.DataFrame(columns=df_columns)
 
 # Function to extract items from the text
-def extract_items_from_text(text):
+def extract_items_from_text(text, store_metadata):
     global df
     #pattern = r"(\*?)(.*?)\s+(\d{13})\s+([\d.]+)\s+([\d.]+)\s+(.*?)\s+([\d.]+)"
     #pattern = r"(\*?)(.*?)\s+(\d{13})\s+([\d.]+)\s+([\d.]+)\s+(.*?)\s+([\d.]+)(?:\n|Total)(?:(.*) - ([\d.]+))?"
     pattern = r"(?P<discount>\*?)(?P<name>.*?)\s+(?P<barcode>\d{13})\s+(?P<price>[\d.]+)\s+(?P<quantity>[\d.]+)\s+(?P<unit>.*?)\s+(?P<total>[\d.]+)(?:\n|Total)(?:(?P<discount_name>.*?) - (?P<discount_total>[\d.]+))?"
     matches = re.finditer(pattern, text, re.MULTILINE)
-    items = []
-    #print(f"Matches found: {len(matches)}")
+    matches_copy, matches = tee(matches)
+    # print amount of matches
+    print("Found {} matches".format(len(list(matches_copy))))
+    items_df = pd.DataFrame(columns=df_columns)
+
     try:
         for match in matches:
-            print(match)
+            print(f"\nMatch: {match}")
             discount = match.group("discount").strip()
             name = match.group("name").strip()
             barcode = match.group("barcode").strip()
@@ -56,12 +61,7 @@ def extract_items_from_text(text):
                 else:
                     discount_total = 0.0
             
-            #item = Item(discount, name, barcode, price, quantity, unit, total, discount_name, discount_total)
-            #items.append(item)
-
-            # add to dataframe
-            
-            data = pd.DataFrame({
+            data = {
                 'Discount': discount,
                 'Name': name,
                 'Barcode': barcode,
@@ -70,17 +70,20 @@ def extract_items_from_text(text):
                 'Unit': unit,
                 'Total': total,
                 'Discount Name': discount_name,
-                'Discount Total': discount_total
-            })
+                'Discount Total': discount_total,
+                'Date': store_metadata['date'],
+                'Time': store_metadata['time'],
+                'Store': store_metadata['store']
+            }
             print(f"Item: {data}")
 
-            #df = df.append(data, ignore_index=True)
+            # add to list
+            items_df = pd.concat([items_df, pd.DataFrame(data, index=[0])], ignore_index=True)
 
-            items.append(data)
-            #items.append((discount, name, barcode, price, quantity, unit, total, discount_name, discount_value))
     except ValueError:
         print(f"Error: {match}")
-    return items
+    df = pd.concat([df, items_df], ignore_index=True)
+    return items_df
 
 # Function to process a PDF file
 def process_pdf_file(file_path):
@@ -91,18 +94,22 @@ def process_pdf_file(file_path):
         for page in pdf.pages:
             text += page.extract_text()
         print(f"Text length: {len(text)}")
-        items = extract_items_from_text(text)
+        store_metadata = extract_store_metadata_from_text(text)
+        items = extract_items_from_text(text, store_metadata)
         print(f"Items found: {len(items)}")
-        for item in items:
-            print(item)
         return items
 
 # Function to generate a bar graph for purchases (sorted by price descending)
 def generate_purchase_bar_graph(items, output_file):
-    sorted_items = sorted(items, key=lambda x: x.total, reverse=True)
-    prices_no_discount = [item.total for item in sorted_items] # prices without discount
-    prices_discount = [item.total - item.discount_total for item in sorted_items] # prices with discount
-    names = [item.name for item in sorted_items]
+    sorted_df = items.sort_values('Total', ascending=False)
+    prices_no_discount = sorted_df['Total'].tolist()  # prices without discount
+    prices_discount = (sorted_df['Total'] - sorted_df['Discount Total']).tolist()  # prices with discount
+    names = sorted_df['Name'].tolist()
+
+    # print sizes
+    #print(f"Prices (No Discount): {len(prices_no_discount)}")
+    #print(f"Prices (Discount): {len(prices_discount)}")
+    #print(f"Names: {len(names)}")
     
     x = np.arange(len(names))  # the label locations
     width = 0.35  # the width of the bars
@@ -112,7 +119,7 @@ def generate_purchase_bar_graph(items, output_file):
     plt.bar(x, prices_discount, width, label='Price (With Discount)', color='blue')
     plt.xlabel('Items', fontsize=24)
     plt.ylabel('Total Price (SEK)', fontsize=24)
-    plt.xticks(x, names, rotation=90, fontsize=18)
+    plt.xticks(x, names, rotation=90, fontsize=14)
     plt.yticks(fontsize=18)
     plt.title('Purchases (Ordered by Price Descending)', fontsize=36)
     plt.legend(fontsize=18)
@@ -121,52 +128,36 @@ def generate_purchase_bar_graph(items, output_file):
     plt.savefig(output_file)
     plt.close()
 
-# Function to generate a pie chart for categorizing items by type
-def generate_purchase_pie_chart(items, output_file):
-    type_count = {}
-    for item in items:
-        item_type = item.name  # Assuming the item type is at index 6, adjust if needed.
-        type_count[item_type] = type_count.get(item_type, 0) + 1
+def extract_store_metadata_from_text(text):
+    name_pattern = r"Kvitto\n(.+)"
+    date_pattern = r"Datum: (\d{4}-\d{2}-\d{2})"
+    time_pattern = r"Tid: (\d{2}:\d{2})"
 
-    types = list(type_count.keys())
-    counts = list(type_count.values())
+    store= re.search(name_pattern, text).group(1)
+    date = re.search(date_pattern, text).group(1)
+    time = re.search(time_pattern, text).group(1)
 
-    plt.figure(figsize=(8, 8))
-    plt.pie(counts, labels=types, autopct='%1.1f%%', startangle=140)
-    plt.axis('equal')
-    plt.title('Purchase Items Categorized by Type')
+    return {"store": store, "date": date, "time": time}
 
-    plt.tight_layout()
-    plt.savefig(output_file)
-    plt.close()
 
 # Main function
 def main():
-    global df
-    input_dir = "src"
-    output_dir = "out"
     os.makedirs(output_dir, exist_ok=True)
 
     pdf_files = [file for file in os.listdir(input_dir) if file.endswith(".pdf")]
-
-    # DataFrame
-    df = pd.DataFrame(columns=['Discount', 'Name', 'Barcode', 'Price', 'Quantity', 'Unit', 'Total Price', 'Discount Name', 'Discount Total', 'Date', 'Time', 'Store'])
 
     for file in pdf_files:
         file_path = os.path.join(input_dir, file)
         items = process_pdf_file(file_path)
 
-        output_file = os.path.splitext(file)[0] + "_graph.png"
+        output_file = os.path.splitext(file)[0] + ".png"
         output_path = os.path.join(output_dir, output_file)
 
         generate_purchase_bar_graph(items, output_path)
         print(f"Bar graph created: {output_path}")
 
- #       pie_output_file = os.path.splitext(file)[0] + "_pie_chart.png"
- #       pie_output_path = os.path.join(output_dir, pie_output_file)
-
- #       generate_purchase_pie_chart(items, pie_output_path)
- #       print(f"Pie chart created: {pie_output_path}")
+    # create a total bar graph from df
+    generate_purchase_bar_graph(df, os.path.join(output_dir, "total.png"))
 
 if __name__ == "__main__":
     main()
